34dd824cdeeed24a95fcbf0932762208f4aeee61
sirval <ohukaiv@gmail.com>
2022-02-26 04:42:19 +0100

    updated Procfile for heruko auto update from git and also error log on dashboard

--- config/app.php
+++ config/app.php
@@ -15,6 +15,10 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    // getting error elog on heruko dashboard 
+    
+    'log'=>'errorlog',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
